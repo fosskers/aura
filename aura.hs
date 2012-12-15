8ef3b37de0340137b7681c60e2ffc30cb81f7d00
Colin Woodbury <colingw@gmail.com>
2012-12-15 21:26:49 +0900

    Made the license message more badass
    - See for yourself.

--- aura.hs
+++ aura.hs
@@ -6,25 +6,35 @@ Copyright 2012 Colin Woodbury <colingw@gmail.com>
 
 This file is part of Aura.
 
-Aura is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Aura is free s
 
-Aura is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+             oftwar
+        e:youcanredist
+     ributeitand/ormodify
+    itunderthetermsoftheGN
+   UGeneralPublicLicenseasp
+  ublishedbytheFreeSoftw
+ areFoundation,either     ver        sio        n3o        fth
+ eLicense,or(atyou       ropti      on)an      ylate      rvers
+ion.Auraisdistr         ibutedi    nthehop    ethatit    willbeu
+ seful,butWITHOUTA       NYWAR      RANTY      ;with      outev
+ entheimpliedwarranty     ofM        ERC        HAN        TAB
+  ILITYorFITNESSFORAPART
+   ICULARPURPOSE.SeetheGNUG
+    eneralPublicLicensefor
+     moredetails.Youshoul
+        dhavereceiveda
+             copyof
 
-You should have received a copy of the GNU General Public License
+the GNU General Public License
 along with Aura.  If not, see <http://www.gnu.org/licenses/>.
 
 -}
 
 import Data.List (intersperse, (\\), nub, sort)
-import Control.Monad (unless)
 import System.Exit (ExitCode, exitWith)
 import System.Environment (getArgs)
+import Control.Monad (unless)
 
 import Aura.State (restoreState, saveState)
 import Aura.Languages
