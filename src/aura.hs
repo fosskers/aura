32a5cd46dcf8e80a9faad7d03fff64dcd9d923de
Colin Woodbury <colingw@gmail.com>
2015-11-18 18:23:45 -0800

    (1.4) Initial commit

--- src/aura.hs
+++ src/aura.hs
@@ -63,7 +63,7 @@ import Aura.Commands.O as O
 type UserInput = ([Flag], [String], [String])
 
 auraVersion :: String
-auraVersion = "1.3.4"
+auraVersion = "1.4.0"
 
 main :: IO a
 main = getArgs >>= prepSettings . processFlags >>= execute >>= exit
