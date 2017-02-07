c47cb75cee37b946bb84bdd096639852ef077d93
Doug Patti <douglas.patti@gmail.com>
2017-02-07 13:47:45 -0500

    move language fields into separate module
    Because the fields use some common names, and because we don't
    explicitly want the rest of aura to use them directly, we can move them
    to an isolated module that gets imported into the `Aura.Languages`
    module. This does mean we will have to move the `Language` type and data
    constructors to a shared module as well.

--- aura/src/Aura/Languages.hs
+++ aura/src/Aura/Languages.hs
@@ -39,30 +39,18 @@ along with Aura.  If not, see <http://www.gnu.org/licenses/>.
 
 -}
 
-module Aura.Languages where
+module Aura.Languages
+    ( module Aura.Languages
+    , Language(..) ) where
 
 import qualified Data.Map.Lazy as Map (Map, (!), fromList, toList, mapWithKey)
 import           Data.Monoid
 
 import           Aura.Colour.Text (cyan, green, red, blue, yellow, magenta, bForeground)
+import           Aura.Languages.Base
+import qualified Aura.Languages.Fields as Fields
 
 ---
-
-data Language = English
-              | Japanese
-              | Polish
-              | Croatian
-              | Swedish
-              | German
-              | Spanish
-              | Portuguese
-              | French
-              | Russian
-              | Italian
-              | Serbian
-              | Norwegian
-              | Indonesia
-                deriving (Eq, Enum, Ord, Read, Show)
 
 translators :: Map.Map Language String
 translators = Map.fromList
@@ -151,278 +139,6 @@ langFromEnv = \case
     "id" -> Indonesia
     _    -> English
 
-----------------------
--- Field labels
-----------------------
-
-package :: Language -> String
-package = \case
-    Japanese   -> "パッケージ"
-    Polish     -> "Pakiet"
-    Croatian   -> "Paket"
-    Swedish    -> "Paket"
-    German     -> "Paket"
-    Spanish    -> "Paquete"
-    Portuguese -> "Pacote"
-    French     -> "Paquet"
-    Russian    -> "Пакет"
-    Italian    -> "Package"
-    Serbian    -> "Пакет"
-    Norwegian  -> "Pakke"
-    Indonesia  -> "Paket"
-    _          -> "Package"
-
-firstInstall :: Language -> String
-firstInstall = \case
-    Japanese   -> "初インストール"
-    Polish     -> "Pierwsza instalacja"
-    Croatian   -> "Prva instalacija"
-    Swedish    -> "Första installation"
-    German     -> "Erste Installation"
-    Spanish    -> "Primera instalación"
-    Portuguese -> "Primeira instalação"
-    French     -> "Première installation"
-    Russian    -> "Первая установка"
-    Italian    -> "Prima installazione"
-    Serbian    -> "Прва инсталација"
-    Norwegian  -> "Første installasjon"
-    Indonesia  -> "Versi sistem"
-    _          -> "First Install"
-
-upgrades :: Language -> String
-upgrades = \case
-    Japanese   -> "アップグレード回数"
-    Polish     -> "Aktualizacje"
-    Croatian   -> "Nadogradnje"
-    Swedish    -> "Uppgraderingar"
-    German     -> "Aktualisierungen"
-    Spanish    -> "Actualizaciones"
-    Portuguese -> "Atualizações"
-    French     -> "Mises à jours"
-    Russian    -> "Обновления"
-    Italian    -> "Upgrades"
-    Serbian    -> "Ажурирања"
-    Norwegian  -> "Oppgraderinger"
-    Indonesia  -> "Tingkatkan"
-    _          -> "Upgrades"
-
-recentActions :: Language -> String
-recentActions = \case
-    Japanese   -> "近況"
-    Polish     -> "Ostatnie akcje"
-    Croatian   -> "Nedavne radnje"
-    Swedish    -> "Nyliga händelser"
-    German     -> "Letzte Aktionen"
-    Spanish    -> "Acciones Recientes"
-    Portuguese -> "Ações Recentes"
-    French     -> "Actions récentes"
-    Russian    -> "Недавние действия"
-    Italian    -> "Azioni recenti"
-    Serbian    -> "Недавне радње"
-    Norwegian  -> "Nylige hendelser"
-    Indonesia  -> "Aksi sekarang"
-    _          -> "Recent Actions"
-
-repository :: Language -> String
-repository = \case
-    Japanese   -> "リポジトリ"
-    Polish     -> "Repozytorium"
-    Croatian   -> "Repozitorij"
-    Swedish    -> "Repository"
-    German     -> "Repository"
-    Spanish    -> "Repositorio"
-    Portuguese -> "Repositório"
-    French     -> "Dépôt"
-    Russian    -> "Репозиторий"
-    Italian    -> "Repository"
-    Serbian    -> "Ризница"
-    Norwegian  -> "Depot"
-    Indonesia  -> "Lumbung"
-    _          -> "Repository"
-
-name :: Language -> String
-name = \case
-    Japanese   -> "名前"
-    Polish     -> "Nazwa"
-    Croatian   -> "Ime"
-    Swedish    -> "Namn"
-    German     -> "Name"
-    Spanish    -> "Nombre"
-    Portuguese -> "Nome"
-    French     -> "Nom"
-    Russian    -> "Название"
-    Italian    -> "Nome"
-    Serbian    -> "Име"
-    Norwegian  -> "Navn"
-    Indonesia  -> "Nama"
-    _          -> "Name"
-
-version :: Language -> String
-version = \case
-    Japanese   -> "バージョン"
-    Polish     -> "Wersja"
-    Croatian   -> "Verzija"
-    Swedish    -> "Version"
-    German     -> "Version"
-    Spanish    -> "Versión"
-    Portuguese -> "Versão"
-    French     -> "Version"
-    Russian    -> "Версия"
-    Italian    -> "Versione"
-    Serbian    -> "Верзија"
-    Norwegian  -> "Versjon"
-    Indonesia  -> "Versi"
-    _          -> "Version"
-
-aurStatus :: Language -> String
-aurStatus = \case
-    Japanese   -> "パッケージ状態"
-    Polish     -> "Status w AUR"
-    Croatian   -> "AUR Stanje"
-    German     -> "AUR-Status"
-    Spanish    -> "Estado en AUR"
-    Portuguese -> "Estado no AUR"
-    French     -> "Statut de AUR"
-    Russian    -> "Статус в AUR"
-    Italian    -> "Stato in AUR"
-    Serbian    -> "Статус у AUR-у"
-    Indonesia  -> "Status AUR"
-    _          -> "AUR Status"
-
--- NEEDS TRANSLATION
-maintainer :: Language -> String
-maintainer = \case
-    Japanese   -> "管理者"
-    Spanish    -> "Mantenedor"
-    French     -> "Mainteneur"
-    Russian    -> "Ответственный"
-    Norwegian  -> "Vedlikeholder"
-    Indonesia  -> "Pemelihara"
-    _          -> "Maintainer"
-
-projectUrl :: Language -> String
-projectUrl = \case
-    Japanese   -> "プロジェクト"
-    Polish     -> "URL Projektu"
-    Croatian   -> "URL Projekta"
-    Swedish    -> "Projekt URL"
-    German     -> "Projekt-URL"
-    Spanish    -> "URL del proyecto"
-    Portuguese -> "URL do projeto"
-    French     -> "URL du projet"
-    Russian    -> "URL проекта"
-    Italian    -> "URL del progetto"
-    Serbian    -> "Страница пројекта"
-    Norwegian  -> "Prosjekt-URL"
-    Indonesia  -> "URL Proyek"
-    _          -> "Project URL"
-
-aurUrl :: Language -> String
-aurUrl = \case
-    Japanese   -> "パッケージページ"
-    Polish     -> "URL w AUR"
-    German     -> "AUR-URL"
-    Spanish    -> "URL de AUR"
-    Portuguese -> "URL no AUR"
-    French     -> "URL AUR"
-    Russian    -> "URL в AUR"
-    Italian    -> "URL AUR"
-    Serbian    -> "Страница у AUR-у"
-    Indonesia  -> "URL AUR"
-    _          -> "AUR URL"
-
-license :: Language -> String
-license = \case
-    Japanese   -> "ライセンス"
-    Polish     -> "Licencja"
-    Croatian   -> "Licenca"
-    Swedish    -> "Licens"
-    German     -> "Lizenz"
-    Spanish    -> "Licencia"
-    Portuguese -> "Licença"
-    French     -> "Licence"
-    Russian    -> "Лицензия"
-    Italian    -> "Licenza"
-    Serbian    -> "Лиценца"
-    Norwegian  -> "Lisens"
-    Indonesia  -> "Lisensi"
-    _          -> "License"
-
-dependsOn :: Language -> String
-dependsOn = \case
-    Japanese   -> "従属パッケージ"
-    Polish     -> "Zależności"
-    Croatian   -> "Zavisnosti"
-    German     -> "Hängt ab von"
-    Spanish    -> "Dependencias"
-    Portuguese -> "Dependências"
-    French     -> "Dépends de"
-    Russian    -> "Зависит от"
-    Italian    -> "Dipende da"
-    Norwegian  -> "Er avhengig av"
-    Indonesia  -> "Bergantung pada"
-    _          -> "Depends On"
-
-buildDeps :: Language -> String
-buildDeps = \case
-    Japanese   -> "作成時従属パ"
-    German     -> "Build-Abhängigkeiten"
-    Spanish    -> "Dependencias de compilación"
-    French     -> "Dépendances de compilation"
-    Russian    -> "Зависимости сборки"
-    Indonesia  -> "Dependensi bangun"
-    _          -> "Build Deps"
-
-votes :: Language -> String
-votes = \case
-    Japanese   -> "投票数"
-    Polish     -> "Głosy"
-    Croatian   -> "Glasovi"
-    Swedish    -> "Röster"
-    German     -> "Stimmen"
-    Spanish    -> "Votos"
-    Portuguese -> "Votos"
-    French     -> "Votes"
-    Russian    -> "Голоса"
-    Italian    -> "Voti"
-    Serbian    -> "Гласови"
-    Norwegian  -> "Stemmer"
-    Indonesia  -> "Suara"
-    _          -> "Votes"
-
-popularity :: Language -> String
-popularity = \case
-    Japanese   -> "人気"
-    _          -> "Popularity"
-
-description :: Language -> String
-description = \case
-    Japanese   -> "概要"
-    Polish     -> "Opis"
-    Croatian   -> "Opis"
-    Swedish    -> "Beskrivning"
-    German     -> "Beschreibung"
-    Spanish    -> "Descripción"
-    Portuguese -> "Descrição"
-    French     -> "Description"
-    Russian    -> "Описание"
-    Italian    -> "Descrizione"
-    Serbian    -> "Опис"
-    Norwegian  -> "Beskrivelse"
-    Indonesia  -> "Deskripsi"
-    _          -> "Description"
-
-makeDeps :: Language -> String
-makeDeps = \case
-    Polish     -> "Zależności Make"
-    Croatian   -> "Make Zavisnosti"
-    German     -> "Make-Abhängigkeiten"
-    Spanish    -> "Dependencias de compilación"
-    Portuguese -> "Depenências de compilação"
-    French     -> "Dépendances de compilation"
-    Russian    -> "Зависимости Make"
-    Indonesia  -> "Dependensi bangun"
-    _          -> "Make Deps"
 
 ----------------------
 -- Aura/Core functions
@@ -1572,10 +1288,10 @@ cleanNotSaved_2 (cyan . show -> s) = \case
 -- Aura/Commands/L functions
 ----------------------------
 logLookUpFields :: Language -> [String]
-logLookUpFields = sequence [ package
-                           , firstInstall
-                           , upgrades
-                           , recentActions
+logLookUpFields = sequence [ Fields.package
+                           , Fields.firstInstall
+                           , Fields.upgrades
+                           , Fields.recentActions
                            ]
 
 reportNotInLog_1 :: Language -> String
@@ -1787,19 +1503,19 @@ getAURPkgInfo_1 = \case
     _          -> "AUR API lookup failed. Please check your connection."
 
 infoFields :: Language -> [String]
-infoFields = sequence [ repository
-                      , name
-                      , version
-                      , aurStatus
-                      , maintainer
-                      , projectUrl
-                      , aurUrl
-                      , license
-                      , dependsOn
-                      , buildDeps
-                      , votes
-                      , popularity
-                      , description
+infoFields = sequence [ Fields.repository
+                      , Fields.name
+                      , Fields.version
+                      , Fields.aurStatus
+                      , Fields.maintainer
+                      , Fields.projectUrl
+                      , Fields.aurUrl
+                      , Fields.license
+                      , Fields.dependsOn
+                      , Fields.buildDeps
+                      , Fields.votes
+                      , Fields.popularity
+                      , Fields.description
                       ]
 
 outOfDateMsg :: Maybe Int -> Language -> String
@@ -1900,12 +1616,12 @@ singleSync_1 (bt -> p) = \case
     _          -> "Syncing " <> p <> " to the local ABS Tree..."
 
 absInfoFields :: Language -> [String]
-absInfoFields = sequence [ repository
-                         , name
-                         , version
-                         , dependsOn
-                         , makeDeps
-                         , description
+absInfoFields = sequence [ Fields.repository
+                         , Fields.name
+                         , Fields.version
+                         , Fields.dependsOn
+                         , Fields.makeDeps
+                         , Fields.description
                          ]
 
 repository_1 :: String -> Language -> String
