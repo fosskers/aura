13c4d5dd7472c0c27958e8d651de0b7373105d7c
Jiehong Ma <email@majiehong.com>
2017-03-10 19:58:09 +0100

    review: split the type definition, and add licence header

--- aura/src/SrcInfoParser.hs
+++ aura/src/SrcInfoParser.hs
@@ -1,71 +1,52 @@
+{-
+
+Copyright 2017 Jiehong Ma <email@majiehong.com>
+
+This file is part of Aura.
+
+Aura is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aura is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aura.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module SrcInfoParser (srcInfoData,
-                      SrcInfo(..),
-                      lineParser,
+module SrcInfoParser (lineParser,
                       Line(..),
                       parseConfig,
                       parseSingleLine,
                       combineJustList,
                       add,
                       merge) where
-
 import Data.Text
 import Data.Int
 import Data.Eq
-import Data.Maybe
-import Data.Functor
-import Text.Megaparsec
-import Text.Megaparsec.String
 import Data.String
 import Data.List
+import Data.Maybe
+import Data.Functor
+import Data.Functor.Identity
+import Text.Megaparsec
+import Text.Megaparsec.String
 import qualified Text.Megaparsec.Lexer as L
 import BasicPrelude
 import Prelude
-
-data SrcInfo = SrcInfo { name        :: Maybe Text
-                       , version     :: Maybe Text
-                       , release     :: Maybe Int
-                       , epoch       :: Maybe Text
-                       , arch        :: Maybe [Text]
-                       , licenses    :: Maybe [Text]
-                       , makeDepends :: Maybe [Text]
-                       , provides    :: Maybe [Text]
-                       , conflicts   :: Maybe [Text]
-                       , sources     :: Maybe [Text]
-                       , md5sums     :: Maybe [Text]
-                       , sha1sums    :: Maybe [Text]
-                       , sha224sums  :: Maybe [Text]
-                       , sha256sums  :: Maybe [Text]
-                       , sha384sums  :: Maybe [Text]
-                       , sha512sums  :: Maybe [Text]
-                       } deriving (Eq, Show)
-
-srcInfoData :: SrcInfo
-srcInfoData = SrcInfo { name        = Nothing
-                      , version     = Nothing
-                      , release     = Nothing
-                      , epoch       = Nothing
-                      , arch        = Nothing
-                      , licenses    = Nothing
-                      , makeDepends = Nothing
-                      , provides    = Nothing
-                      , conflicts   = Nothing
-                      , sources     = Nothing
-                      , md5sums     = Nothing
-                      , sha1sums    = Nothing
-                      , sha224sums  = Nothing
-                      , sha256sums  = Nothing
-                      , sha384sums  = Nothing
-                      , sha512sums  = Nothing
-                      }
+import SrcInfoType
 
 data Line = Line { parameter :: String
                  , value     :: String
                  } deriving (Eq, Show)
-
-type Lines = [Line]
 
 skip :: Parser ()
 skip = L.space (void spaceChar) commentLine commentLine
@@ -145,9 +126,6 @@ combineJustList a b =
 
 merge :: [SrcInfo] -> SrcInfo
 merge = BasicPrelude.foldl add srcInfoData
-
-removeEmptyLines :: Line -> Bool
-removeEmptyLines line = parameter line /= ""
 
 parseConfig :: String -> SrcInfo
 parseConfig input = do
--- aura/tests/SrcInfoParserTest.hs
+++ aura/tests/SrcInfoParserTest.hs
@@ -1,3 +1,24 @@
+{-
+
+Copyright 2017 Jiehong Ma <email@majiehong.com>
+
+This file is part of Aura.
+
+Aura is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aura is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aura.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 module SrcInfoParserTest where
 
 import Test.Tasty
@@ -7,6 +28,7 @@ import Data.List
 import Data.Text
 import SrcInfoParser
 import Prelude
+import SrcInfoType
 
 tests :: TestTree
 tests = testGroup "Src Info Parser"
