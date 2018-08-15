50525a88961ef9f733546b65c56318fe37c3bf1b
Colin Woodbury <colin@fosskers.ca>
2018-08-15 10:36:52 -0700

    (aur) AUR calls can fail

--- aur/tests/Test.hs
+++ aur/tests/Test.hs
@@ -18,10 +18,10 @@ suite m = testGroup "RPC Calls"
   ]
 
 infoTest :: Manager -> Assertion
-infoTest m = info m ["aura"] >>= assertBool "Good package" . not . null
+infoTest m = info m ["aura"] >>= \x -> (not . null <$> x) @?= Just True
 
 infoTest' :: Manager -> Assertion
-infoTest' m = info m ["aura1234567"] >>= assertBool "Bad package" . null
+infoTest' m = info m ["aura1234567"] >>= \x -> (null <$> x) @?= Just True
 
 searchTest :: Manager -> Assertion
 searchTest m = search m "aura" >>= assertBool "Good search" . not . null
