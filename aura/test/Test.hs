bd036419d45abfd06d41aff0c0d9d2e0e7fe3dfd
Colin Woodbury <colin@fosskers.ca>
2018-08-22 16:39:31 -0700

    (aura) Fix tests

--- aura/test/Test.hs
+++ aura/test/Test.hs
@@ -70,7 +70,7 @@ suite conf pb = testGroup "Unit Tests"
         -- pPrintNoColor $ map (first prettyText) . bannedTerms <$> ppb
         -- pPrintNoColor ppb
         assertBool "Failed to parse" $ isJust ppb
-    , testCase "Detecting banned terms" $ (length . bannedTerms <$> ppb) @?= Just 7
+    , testCase "Detecting banned terms" $ (length . bannedTerms <$> ppb) @?= Just 5
     ]
   ]
   where ppb = parsedPB pb
