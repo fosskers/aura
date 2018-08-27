5589bd86d6b6510dc4d896b273ffbfb4122906de
Colin Woodbury <colin@fosskers.ca>
2018-08-26 18:15:22 -0700

    (aur-security) Download PKGBUILD directly, avoiding RPC calls

--- aur-security/Main.hs
+++ aur-security/Main.hs
@@ -2,18 +2,14 @@
 
 module Main ( main ) where
 
-import           Aura.Packages.AUR (aurLookup)
+import           Aura.Pkgbuild.Fetch (getPkgbuild)
 import           Aura.Pkgbuild.Security (parsedPB, bannedTerms)
 import           Aura.Types
 import           BasePrelude
 import           Control.Compactable (fmapEither)
-import           Control.Concurrent.STM (atomically)
-import           Control.Concurrent.STM.TQueue (flushTQueue)
-import           Control.Concurrent.Throttled (throttleMaybe)
+import           Control.Concurrent.Async (mapConcurrently)
 import           Control.Error.Util (note)
 import           Data.List.Split (chunksOf)
-import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import           Language.Bash.Pretty (prettyText)
@@ -27,28 +23,23 @@ main :: IO ()
 main = do
   m   <- newManager tlsManagerSettings
   pns <- sort . map PkgName . T.lines <$> T.readFile "aur-security/packages.txt"
-  let grouped = mapMaybe (NES.fromSet . S.fromDistinctAscList) $ chunksOf 50 pns
-  putStrLn $ printf "Read %d package names." (length pns)
-  throttleMaybe (\_ ps -> aurLookup m ps) grouped >>= \case
-    Left _  -> putStrLn "AUR CONNECTION ERROR"
-    Right q -> do
-      (nons, bs) <- bimap fold (S.filter p . fold) . unzip <$> atomically (flushTQueue q)
-      unless (null nons) $ do
-        putStrLn "The following weren't identified as AUR packages:"
-        traverse_ print nons
-      putStrLn "Analysing legal packages..."
-      let (unparsed, parsed) = fmapEither f $ toList bs
-      unless (null unparsed) $ do
-        putStrLn $ printf "The PKGBUILDs of %d packages couldn't be parsed. They were:" (length unparsed)
-        traverse_ print unparsed
-      let bads = mapMaybe g parsed
-      unless (null bads) $ do
-        putStrLn $ printf "%d PKGBUILDs contained banned bash terms. They were:" (length bads)
-        traverse_ pPrintNoColor bads
-      putStrLn "Done."
-        where p (Buildable { name, base })     = name == base
-              f (Buildable { name, pkgbuild }) = note name . fmap (name,) $ parsedPB pkgbuild
-              g = traverse (maybeList . map (first prettyText) . bannedTerms)
+  let !len = length pns
+  putStrLn $ printf "Read %d package names." len
+  q <- mapConcurrently (traverse (\pn -> fmap (pn,) . note pn <$> getPkgbuild m pn)) $ chunksOf (len `div` 16) pns
+  let (nopbs, pbs) = partitionEithers $ fold q
+  unless (null nopbs) . putStrLn $ printf "PKGBUILDs couldn't be found for %d packages." (length nopbs)
+  putStrLn "Analysing legal packages..."
+  let (unparsed, parsed) = fmapEither f pbs
+  unless (null unparsed) $ do
+    putStrLn $ printf "The PKGBUILDs of %d packages couldn't be parsed. They were:" (length unparsed)
+    traverse_ print unparsed
+  let !bads = mapMaybe g parsed
+  unless (null bads) $ do
+    putStrLn $ printf "%d PKGBUILDs contained banned bash terms. They were:" (length bads)
+    traverse_ pPrintNoColor bads
+  putStrLn "Done."
+    where f pair@(pn, _) = note pn $ traverse parsedPB pair
+          g = traverse (maybeList . map (first prettyText) . bannedTerms)
 
 maybeList :: [a] -> Maybe [a]
 maybeList [] = Nothing
