fd3bbabbbc46ffb5411167261637dc57a7fc4b2f
Joe Kachmar <joseph.kachmar@gmail.com>
2019-01-19 01:04:25 -0500

    Uses fused-effects for Aura.Commands.O

--- aura/lib/Aura/Commands/O.hs
+++ aura/lib/Aura/Commands/O.hs
@@ -14,14 +14,12 @@
 
 module Aura.Commands.O ( displayOrphans, adoptPkg ) where
 
-import           Aura.Core (liftEitherM, orphans, sudo)
+import           Aura.Core (liftEitherM, orphans, sendM, sudo)
 import           Aura.Pacman (pacman)
 import           Aura.Settings (Settings)
 import           Aura.Types
 import           BasePrelude
-import           Control.Monad.Freer
-import           Control.Monad.Freer.Error
-import           Control.Monad.Freer.Reader
+import           Control.Effect
 import           Data.Generics.Product (field)
 import           Data.Set.NonEmpty (NonEmptySet)
 import qualified Data.Text.IO as T
@@ -34,5 +32,10 @@ displayOrphans :: IO ()
 displayOrphans = orphans >>= traverse_ (T.putStrLn . view (field @"name"))
 
 -- | Identical to @-D --asexplicit@.
-adoptPkg :: (Member (Reader Settings) r, Member (Error Failure) r, Member IO r) => NonEmptySet PkgName -> Eff r ()
-adoptPkg pkgs = sudo . liftEitherM . pacman $ ["-D", "--asexplicit"] <> asFlag pkgs
+adoptPkg :: ( Monad m, Carrier sig m
+            , Member (Reader Settings) sig
+            , Member (Error Failure) sig
+            , Member (Lift IO) sig
+            )
+         => NonEmptySet PkgName -> m ()
+adoptPkg pkgs = sudo . liftEitherM . sendM . pacman $ ["-D", "--asexplicit"] <> asFlag pkgs
