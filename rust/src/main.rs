abb582515731b021d2057613cbf5ad184f4fd0fb
Colin Woodbury <colin@fosskers.ca>
2020-10-28 15:48:38 -0700

    [aura] Use `rayon` and `r2d2` for concurrent alpm access

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,16 @@
+use alpm::Db;
 use alpm::{Alpm, SigLevel};
 use clap::{crate_authors, crate_version, App, AppSettings, Arg};
 use curl::easy::Easy;
 use indicatif::ProgressBar;
+use r2d2::{ManageConnection, Pool, PooledConnection};
+use rayon::prelude::*;
 
 #[derive(Debug)]
 enum Error {
     ALPM(alpm::Error),
     CURL(curl::Error),
+    R2D2(r2d2::Error),
     Other(&'static str),
 }
 
@@ -15,6 +19,7 @@ impl std::fmt::Display for Error {
         match self {
             Error::ALPM(e) => e.fmt(f),
             Error::CURL(e) => e.fmt(f),
+            Error::R2D2(e) => e.fmt(f),
             Error::Other(s) => write!(f, "{}", s),
         }
     }
@@ -25,13 +30,69 @@ impl std::error::Error for Error {
         match self {
             Error::ALPM(e) => Some(e),
             Error::CURL(e) => Some(e),
+            Error::R2D2(e) => Some(e),
             Error::Other(_) => None,
         }
     }
 }
 
+// TODO Consider string slices later.
+struct AlpmManager {
+    root: String,
+    db_path: String,
+    sync_dbs: Vec<String>,
+}
+
+impl Default for AlpmManager {
+    fn default() -> Self {
+        AlpmManager {
+            root: "/".to_string(),
+            db_path: "/var/lib/pacman/".to_string(),
+            sync_dbs: vec![
+                "core".to_string(),
+                "extra".to_string(),
+                "community".to_string(),
+            ],
+        }
+    }
+}
+
+impl ManageConnection for AlpmManager {
+    type Connection = alpm::Alpm;
+    type Error = alpm::Error;
+
+    fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        // From `libalpm.h`:
+        //
+        // > Initializes the library.
+        // > Creates handle, connects to database and creates lockfile.
+        // > This must be called before any other functions are called.
+        let alpm = Alpm::new(self.root.clone(), self.db_path.clone())?;
+        self.sync_dbs
+            .iter()
+            .map(|s| alpm.register_syncdb(s, SigLevel::USE_DEFAULT))
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(alpm)
+    }
+
+    // Are we still connected to the database? Docs say that it's not
+    // unreasonable to actually try a query to see what happens.
+    fn is_valid(&self, _: &mut Self::Connection) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    // TODO This is supposed to be a fast check. What does libalpm provide?
+    fn has_broken(&self, _: &mut Self::Connection) -> bool {
+        false
+    }
+}
+
 fn main() -> Result<(), Error> {
-    let alpm = Alpm::new("/", "/var/lib/pacman/").map_err(Error::ALPM)?;
+    let manager = AlpmManager::default();
+    let pool = Pool::builder()
+        .max_size(4)
+        .build(manager)
+        .map_err(Error::R2D2)?;
     let ver: &str = &format!("{} - libalpm {}", crate_version!(), alpm::version());
 
     let args = App::new("aura")
@@ -100,7 +161,8 @@ fn main() -> Result<(), Error> {
                 .unwrap_or_else(|| Vec::new());
 
             if matches.is_present("info") {
-                let db = alpm.localdb();
+                let conn: PooledConnection<AlpmManager> = pool.get().unwrap();
+                let db = conn.localdb();
                 match packages.as_slice() {
                     // Fetch all packages.
                     [] => {
@@ -117,49 +179,46 @@ fn main() -> Result<(), Error> {
                     }
                 }
             } else {
-                // Look up the package.
-                let sync_dbs = init_sync_dbs(&alpm).map_err(Error::ALPM)?;
-                let p = packages
-                    .first()
-                    .ok_or_else(|| Error::Other("No packages!"))?;
-                println!("Looking for {}...", p);
-                println!("# of Sync DBs: {}", sync_dbs.len());
-                let pkg = sync_dbs
-                    .into_iter()
-                    .filter_map(|db| db.pkg(*p).ok())
-                    .next()
-                    .ok_or_else(|| Error::Other("No db matches!"))?;
-
-                // Form the download URL.
-                let db_str = pkg
-                    .db()
-                    .map(|db| db.name())
-                    .ok_or(Error::Other("Unknown DB!"))?;
-                let arch = pkg.arch().ok_or(Error::Other("Unknown architecture!"))?;
-                let url = format!(
-                    "https://mirror.f4st.host/archlinux/{}/os/{}/{}-{}-{}.pkg.tar.zst",
-                    db_str,
-                    arch,
-                    pkg.name(),
-                    pkg.version(),
-                    arch,
-                );
-
-                // Set up the tarball download.
-                let total = pkg.download_size() as u64;
-                let pb = ProgressBar::new(total);
-                let mut handle = Easy::new();
-                handle.progress(true).map_err(Error::CURL)?;
-                handle.url(&url).map_err(Error::CURL)?;
-                handle
-                    .progress_function(move |_, dld, _, _| {
-                        pb.set_position(dld as u64);
-                        true
+                packages
+                    .into_par_iter()
+                    .map_with(pool, |pul, pkg| {
+                        let conn: PooledConnection<AlpmManager> = pul.get().unwrap();
+                        let dbs = conn.syncdbs();
+                        dbs.iter()
+                            .filter_map(|db| package_url(&db, pkg).ok())
+                            .next()
                     })
-                    .map_err(Error::CURL)?;
+                    .filter_map(|url| url)
+                    .for_each(|url| {
+                        println!("Fetching {}", url);
+                        let mut handle = Easy::new();
+                        // handle.progress(true).map_err(Error::CURL)?;
+                        // handle.url(&url).map_err(Error::CURL)?;
+                        handle.progress(true).unwrap();
+                        handle.url(&url).unwrap();
+                        handle.perform().unwrap();
+                        println!("Done {}", url);
+                    });
 
-                // Download the package tarball.
-                handle.perform().map_err(Error::CURL)?;
+                // // Set up the tarball download.
+                // let total = pkg.download_size() as u64;
+                // let pb = ProgressBar::new(total);
+                // let mut handle = Easy::new();
+                // handle.progress(true).map_err(Error::CURL)?;
+                // handle.url(&url).map_err(Error::CURL)?;
+                // handle
+                //     .progress_function(move |_, dld, _, _| {
+                //         let du = dld as u64;
+                //         pb.set_position(du);
+                //         if du == total {
+                //             pb.finish_with_message("");
+                //         }
+                //         true
+                //     })
+                //     .map_err(Error::CURL)?;
+
+                // // Download the package tarball.
+                // handle.perform().map_err(Error::CURL)?;
             }
         }
         _ => unreachable!(),
@@ -168,13 +227,25 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-/// Handles to the official repos must be initialized before they can be
-/// queried.
-fn init_sync_dbs(alpm: &Alpm) -> alpm::Result<Vec<alpm::Db>> {
-    let names = vec!["core", "extra", "community"];
+/// Given the name of some package, attempt to form the download URL of its tarball.
+fn package_url(db: &Db, pkg: &str) -> Result<String, Error> {
+    // Look up the package.
+    let p = db.pkg(pkg).map_err(Error::ALPM)?;
 
-    names
-        .into_iter()
-        .map(|s| alpm.register_syncdb(s, SigLevel::USE_DEFAULT))
-        .collect()
+    // Form the download URL.
+    let db_str = p
+        .db()
+        .map(|db| db.name())
+        .ok_or(Error::Other("Unknown DB!"))?;
+    let arch = p.arch().ok_or(Error::Other("Unknown architecture!"))?;
+    let url = format!(
+        "https://mirror.f4st.host/archlinux/{}/os/{}/{}-{}-{}.pkg.tar.zst",
+        db_str,
+        arch,
+        p.name(),
+        p.version(),
+        arch,
+    );
+
+    Ok(url)
 }
