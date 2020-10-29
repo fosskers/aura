08ed51327ff2664802f6ee5f79c31ddd98687dba
Colin Woodbury <colin@fosskers.ca>
2020-10-29 13:47:16 -0700

    [aura] Use `clap` auto-derivation instead
    This is much cleaner, and also more correct. It fixed a few config issues I
    couldn't figure out with the manual approach.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 use alpm::{Alpm, Package, SigLevel};
-use clap::{crate_authors, crate_version, App, AppSettings, Arg, ArgGroup};
+use clap::{crate_version, AppSettings, Clap};
 use curl::easy::Easy;
 use fluent::{FluentBundle, FluentResource};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
@@ -10,7 +10,7 @@ use unic_langid::langid;
 
 #[derive(Debug)]
 enum Error {
-    ALPM(alpm::Error),
+    // ALPM(alpm::Error),
     // CURL(curl::Error),
     R2D2(r2d2::Error),
     Other(&'static str),
@@ -19,7 +19,7 @@ enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Error::ALPM(e) => e.fmt(f),
+            // Error::ALPM(e) => e.fmt(f),
             // Error::CURL(e) => e.fmt(f),
             Error::R2D2(e) => e.fmt(f),
             Error::Other(s) => write!(f, "{}", s),
@@ -30,7 +30,7 @@ impl std::fmt::Display for Error {
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::ALPM(e) => Some(e),
+            // Error::ALPM(e) => Some(e),
             // Error::CURL(e) => Some(e),
             Error::R2D2(e) => Some(e),
             Error::Other(_) => None,
@@ -38,11 +38,41 @@ impl std::error::Error for Error {
     }
 }
 
-/// Localization languages for users output.
-// enum Language {
-//     English,
-//     Japanese,
-// }
+#[derive(Clap, Debug)]
+#[clap(version = crate_version!(),
+       author = "Colin Woodbury",
+       about = "Install and manage Arch Linux and AUR packages",
+       setting = AppSettings::VersionlessSubcommands)]
+struct Args {
+    /// Output in English.
+    #[clap(group = "lang", long, global = true)]
+    english: bool,
+    /// Output in Japanese.
+    #[clap(group = "lang", long, global = true)]
+    japanese: bool,
+    /// Less verbose output.
+    #[clap(long, short, global = true)]
+    quiet: bool,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+enum SubCommand {
+    Sync(Sync),
+}
+
+/// Synchronize packages.
+#[derive(Clap, Debug)]
+#[clap(short_flag = 'S', long_flag = "sync")]
+struct Sync {
+    #[clap(long, short, about = "Search for packages via REGEX.")]
+    search: bool,
+    #[clap(long, short, about = "Look up a specific package.")]
+    info: bool,
+    #[clap(about = "Packages to search/install.")]
+    packages: Vec<String>,
+}
 
 // TODO Consider string slices later.
 struct AlpmManager {
@@ -97,6 +127,9 @@ impl ManageConnection for AlpmManager {
 }
 
 fn main() -> Result<(), Error> {
+    // Parse commandline args before anything else.
+    let args = Args::parse();
+
     // Pooled connections to ALPM.
     let manager = AlpmManager::default();
     let pool = Pool::builder()
@@ -113,190 +146,97 @@ fn main() -> Result<(), Error> {
         .map_err(|_| Error::Other("Couldn't parse English localisations."))?;
     let ja_res = FluentResource::try_new(ja_msg.to_owned())
         .map_err(|_| Error::Other("Couldn't parse Japanese localisations."))?;
-    let mut bundle = FluentBundle::new(&[english, japanese]);
-    bundle
-        .add_resource(&en_res)
-        .map_err(|_| Error::Other("Failed to add English to bundle."))?;
-    // bundle
-    //     .add_resource(&ja_res)
-    //     .map_err(|_| Error::Other("Failed to add Japanese to bundle."))?;
 
-    let ver: &str = &format!("{} - libalpm {}", crate_version!(), alpm::version());
+    let bundle = if args.japanese {
+        let mut bundle = FluentBundle::new(&[japanese]);
+        bundle
+            .add_resource(&ja_res)
+            .map_err(|_| Error::Other("Failed to add Japanese to bundle."))?;
+        bundle
+    } else {
+        let mut bundle = FluentBundle::new(&[english]);
+        bundle
+            .add_resource(&en_res)
+            .map_err(|_| Error::Other("Failed to add English to bundle."))?;
+        bundle
+    };
 
-    let args = App::new("aura")
-        .author(crate_authors!())
-        .version(ver)
-        .about("Install and manage Arch Linux and AUR packages.")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .arg(
-            Arg::new("english")
-                .long("english")
-                .about("Output is in English.")
-                .global(true),
-        )
-        .arg(
-            Arg::new("japanese")
-                .long("japanese")
-                .alias("日本語")
-                .about("Output is in Japanese.")
-                .global(true),
-        )
-        .group(
-            ArgGroup::new("language")
-                .args(&["english", "japanese"])
-                .required(true),
-        )
-        // .subcommand(
-        //     App::new("query")
-        //         .short_flag('Q')
-        //         .long_flag("query")
-        //         .about("Query the package database.")
-        //         .arg(
-        //             Arg::new("search")
-        //                 .short('s')
-        //                 .long("search")
-        //                 .about("Search locally installed packages.")
-        //                 .multiple_values(true),
-        //         )
-        //         .arg(
-        //             Arg::new("info")
-        //                 .long("info")
-        //                 .short('i')
-        //                 .conflicts_with("search")
-        //                 .about("view package information")
-        //                 .multiple_values(true),
-        //         ),
-        // )
-        .subcommand(
-            App::new("sync")
-                .short_flag('S')
-                .long_flag("sync")
-                .about("Synchronize packages.")
-                .arg(Arg::new("quiet").long("quiet").short('q').about("Shhh"))
-                .arg(
-                    Arg::new("search")
-                        .short('s')
-                        .long("search")
-                        .conflicts_with("info")
-                        .about("Search repositories for a matching regex."),
-                )
-                .arg(
-                    Arg::new("info")
-                        .short('i')
-                        .long("info")
-                        .conflicts_with("search")
-                        .about("View package information."),
-                )
-                .arg(
-                    Arg::new("package")
-                        .about("Packages to install.")
-                        .multiple(true)
-                        // .required_unless_present("search")
-                        .takes_value(true),
-                ),
-        )
-        .get_matches();
+    match args.subcmd {
+        SubCommand::Sync(s) if s.info => println!("Info!"),
+        SubCommand::Sync(s) if s.search => println!("Search!"),
+        SubCommand::Sync(s) => {
+            // Display localized message.
+            let pat = bundle.get_message("downloading").unwrap().value.unwrap();
+            let mut err = vec![];
+            let msg = bundle.format_pattern(&pat, None, &mut err);
+            println!("{}", msg);
 
-    println!("{:#?}", args);
+            // Look up each package in the ALPM database.
+            let packages: Vec<(String, String, u64)> = s
+                .packages
+                .par_iter()
+                .map_with(pool, |pul, pkg| {
+                    let conn: PooledConnection<AlpmManager> = pul.get().unwrap();
+                    let dbs = conn.syncdbs();
+                    dbs.iter()
+                        .filter_map(|db| db.pkg(pkg).ok())
+                        .filter_map(|p| {
+                            let total = p.download_size() as u64;
+                            package_url(&p)
+                                .ok()
+                                .map(|u| (p.name().to_string(), u, total))
+                        })
+                        .next()
+                })
+                .filter_map(|o| o)
+                .collect();
 
-    match args.subcommand() {
-        Some(("sync", matches)) => {
-            // The actual package names.
-            let pkg_names: Vec<_> = matches
-                .values_of("package")
-                .map(|v| v.collect())
-                .unwrap_or_else(|| Vec::new());
+            // Set up download progress display.
+            let m = MultiProgress::new();
+            let spinners: Vec<ProgressBar> = packages
+                .iter()
+                .map(|(p_name, _, total)| {
+                    let pb = ProgressBar::new(*total);
+                    // TODO Make the spacing dynamic based on the package names.
+                    let template = format!(
+                        "{:<8} [{{wide_bar:.cyan/blue}}] {{bytes}}/{{total_bytes}} ({{eta}})",
+                        p_name
+                    );
+                    let style = ProgressStyle::default_bar()
+                        .template(&template)
+                        .progress_chars("#>-");
+                    pb.set_style(style);
+                    m.add(pb)
+                })
+                .collect();
+            thread::spawn(move || m.join());
 
-            if matches.is_present("info") {
-                let conn: PooledConnection<AlpmManager> = pool.get().unwrap();
-                let db = conn.localdb();
-                match pkg_names.as_slice() {
-                    // Fetch all packages.
-                    [] => {
-                        for p in db.pkgs() {
-                            println!("{} - {}", p.name(), p.version());
-                        }
-                    }
-                    // Do lookups for each package.
-                    _ => {
-                        for p in pkg_names {
-                            let pkg = db.pkg(p).map_err(Error::ALPM)?;
-                            println!("{} - {}", pkg.name(), pkg.version());
-                        }
-                    }
-                }
-            } else {
-                let pat = bundle.get_message("downloading").unwrap().value.unwrap();
-                let mut err = vec![];
-                let msg = bundle.format_pattern(&pat, None, &mut err);
-                println!("{}", msg);
+            // Download the packages concurrently while displaying progress.
+            packages
+                .into_par_iter()
+                .zip(spinners)
+                .for_each(|((_, url, total), pb)| {
+                    // TODO Could cache the `Easy` sessions and use
+                    // `Easy::reset` between each use!
+                    let mut handle = Easy::new();
+                    // handle.progress(true).map_err(Error::CURL)?;
+                    // handle.url(&url).map_err(Error::CURL)?;
+                    handle.progress(true).unwrap();
+                    handle.url(&url).unwrap();
+                    handle
+                        .progress_function(move |_, dld, _, _| {
+                            let du = dld as u64;
+                            pb.set_position(du);
+                            if du == total {
+                                pb.finish();
+                            }
+                            true
+                        })
+                        .unwrap();
 
-                let packages: Vec<(String, String, u64)> = pkg_names
-                    .into_par_iter()
-                    .map_with(pool, |pul, pkg| {
-                        let conn: PooledConnection<AlpmManager> = pul.get().unwrap();
-                        let dbs = conn.syncdbs();
-                        dbs.iter()
-                            .filter_map(|db| db.pkg(pkg).ok())
-                            .filter_map(|p| {
-                                let total = p.download_size() as u64;
-                                package_url(&p)
-                                    .ok()
-                                    .map(|u| (p.name().to_string(), u, total))
-                            })
-                            .next()
-                    })
-                    .filter_map(|o| o)
-                    .collect();
-
-                // Set up download progress display.
-                let m = MultiProgress::new();
-                let spinners: Vec<ProgressBar> = packages
-                    .iter()
-                    .map(|(p_name, _, total)| {
-                        let pb = ProgressBar::new(*total);
-                        // TODO Make the spacing dynamic based on the package names.
-                        let template = format!(
-                            "{:<8} [{{wide_bar:.cyan/blue}}] {{bytes}}/{{total_bytes}} ({{eta}})",
-                            p_name
-                        );
-                        let style = ProgressStyle::default_bar()
-                            .template(&template)
-                            .progress_chars("#>-");
-                        pb.set_style(style);
-                        m.add(pb)
-                    })
-                    .collect();
-                thread::spawn(move || m.join());
-
-                packages
-                    .into_par_iter()
-                    .zip(spinners)
-                    .for_each(|((_, url, total), pb)| {
-                        // TODO Could cache the `Easy` sessions and use
-                        // `Easy::reset` between each use!
-                        let mut handle = Easy::new();
-                        // handle.progress(true).map_err(Error::CURL)?;
-                        // handle.url(&url).map_err(Error::CURL)?;
-                        handle.progress(true).unwrap();
-                        handle.url(&url).unwrap();
-                        handle
-                            .progress_function(move |_, dld, _, _| {
-                                let du = dld as u64;
-                                pb.set_position(du);
-                                if du == total {
-                                    pb.finish();
-                                }
-                                true
-                            })
-                            .unwrap();
-
-                        handle.perform().unwrap();
-                    });
-            }
+                    handle.perform().unwrap();
+                });
         }
-        _ => unreachable!(),
     }
 
     Ok(())
