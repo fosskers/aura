927ea28765170f2aeec12a976c6c75f995df1710
Colin Woodbury <colin@fosskers.ca>
2020-11-04 15:12:51 -0800

    [aura] Add struct for `-U`

--- rust/aura/src/flags.rs
+++ rust/aura/src/flags.rs
@@ -38,7 +38,7 @@ pub enum SubCmd {
     Remove,
     Sync(Sync),
     TestDeps(TestDeps),
-    Upgrade,
+    Upgrade(Upgrade),
     // --- Aura Commands --- //
     AurSync,
     Backup,
@@ -288,6 +288,99 @@ pub struct TestDeps {
     /// Operate on a mounted guests system (root-only).
     #[clap(long)]
     sysroot: bool,
-    /// Packages to search/install.
+    /// Dependencies to check.
     packages: Vec<String>,
+}
+
+/// ???
+#[derive(Clap, Debug)]
+#[clap(short_flag = 'U', long_flag = "upgrade")]
+pub struct Upgrade {
+    /// Set an alternate database location.
+    #[clap(long, short = 'b', value_name = "path", display_order = 1)]
+    dbpath: Option<String>,
+    /// Skip dependency version checks (-dd to skip all checks).
+    #[clap(long, short = 'd', display_order = 1)]
+    nodeps: bool,
+    /// Print the targets instead of performing the operation.
+    #[clap(long, short, display_order = 1)]
+    print: bool,
+    /// Set an alternate installation root.
+    #[clap(long, short, value_name = "path", display_order = 1)]
+    root: Option<String>,
+    /// Be verbose.
+    #[clap(long, short, display_order = 1)]
+    verbose: bool,
+    /// Set an alternate architecture.
+    #[clap(long)]
+    arch: Option<String>,
+    /// Install packages as non-explicitly installed.
+    #[clap(long)]
+    asdeps: bool,
+    /// Install pacakges as explicitly installed.
+    #[clap(long)]
+    asexplicit: bool,
+    /// Add a virtual package to satisfy dependencies.
+    #[clap(long, value_name = "package=version")]
+    assumed_installed: Option<String>,
+    /// Set an alternate package cache location.
+    #[clap(long, value_name = "dir")]
+    cachedir: Option<String>,
+    /// Colorize the output.
+    #[clap(long, value_name = "when", possible_values = &["always", "never", "auto"])]
+    color: Option<String>,
+    /// Set an alternate Pacman configuration file.
+    #[clap(long, value_name = "path")]
+    config: Option<String>,
+    /// Always ask for confirmation.
+    #[clap(long)]
+    confirm: bool,
+    /// Only modify database entries, not package files.
+    #[clap(long)]
+    dbonly: bool,
+    /// Display Pacman debug messages.
+    #[clap(long)]
+    debug: bool,
+    // TODO Here too, this shouldn't be relevant.
+    /// Use relaxed timeouts for download.
+    #[clap(long)]
+    disable_download_timeout: bool,
+    /// Set an alternate home directory for GnuPG.
+    #[clap(long, value_name = "path")]
+    gpgdir: Option<String>,
+    /// Set an alternate hook location.
+    #[clap(long, value_name = "dir")]
+    hookdir: Option<String>,
+    /// Ignore a package upgrade (can be used more than once).
+    #[clap(long, value_name = "pkg")]
+    ignore: Option<String>,
+    /// Ignore a group ugrade (can be used more than once).
+    #[clap(long, value_name = "grp")]
+    ignoregroup: Option<String>,
+    /// Set an alternate log file.
+    #[clap(long, value_name = "path")]
+    logfile: Option<String>,
+    /// Do not reinstall up to date packages.
+    #[clap(long)]
+    needed: bool,
+    /// Do not ask for any confirmation.
+    #[clap(long)]
+    noconfirm: bool,
+    /// Do not show a progress bar when downloading files.
+    #[clap(long)]
+    noprogressbar: bool,
+    /// Do not execute the install scriptlet if one exists.
+    #[clap(long)]
+    noscriptlet: bool,
+    /// Overwrite conflicting files (can be used more than once.)
+    #[clap(long, value_name = "path")]
+    overwrite: Option<String>,
+    /// Specify how the targets should be printed.
+    #[clap(long, value_name = "string")]
+    print_format: Option<String>,
+    /// Operate on a mounted guests system (root-only).
+    #[clap(long)]
+    sysroot: bool,
+    /// Tarballed packages to install.
+    files: Vec<String>,
 }
