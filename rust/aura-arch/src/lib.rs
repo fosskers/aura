8e6bbcabf0b6b2acd06594b974f4ac264b6b600a
Colin Woodbury <colin@fosskers.ca>
2020-11-11 15:54:20 -0800

    [aura] Depend on `aura-arch`

--- rust/aura-arch/src/lib.rs
+++ rust/aura-arch/src/lib.rs
@@ -1,3 +1,12 @@
+// TODO Could this be an iterator?
+/// All orphaned packages.
+///
+/// An orphan is a package that was installed as a dependency, but whose parent
+/// package is no longer installed.
+pub fn orphans() -> Vec<String> {
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
