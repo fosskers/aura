fd7641547954cef604408e2711c8431f5dfc2c60
Colin Woodbury <colin@fosskers.ca>
2020-11-26 08:33:58 -0800

    [core] cache: Reading the size of a directory

--- rust/aura-core/src/cache.rs
+++ rust/aura-core/src/cache.rs
@@ -32,3 +32,14 @@ pub fn search(path: &Path, term: String) -> Result<CacheMatches, std::io::Error>
     let read_dir = path.read_dir()?;
     Ok(CacheMatches { read_dir, term })
 }
+
+/// The number of files and all bytes consumed by a given `Path`.
+pub fn size(path: &Path) -> Result<(u64, u64), std::io::Error> {
+    let res = path
+        .read_dir()?
+        .filter_map(|de| de.ok())
+        .filter_map(|de| de.metadata().ok())
+        .map(|meta| meta.len())
+        .fold((0, 0), |(ac, al), l| (ac + 1, al + l));
+    Ok(res)
+}
