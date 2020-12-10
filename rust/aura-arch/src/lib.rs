b0112d959a1b16b1b1eb00ca8d07f0dee7a1575b
Colin Woodbury <colin@fosskers.ca>
2020-12-10 10:10:09 -0800

    [arch] `Officials` iterator

--- rust/aura-arch/src/lib.rs
+++ rust/aura-arch/src/lib.rs
@@ -9,6 +9,28 @@ pub const DEFAULT_PAC_CONF: &str = "/etc/pacman.conf";
 /// The default filepath of the Makepkg configuration.
 pub const DEFAULT_MAKEPKG_CONF: &str = "/etc/makepkg.conf";
 
+/// An `Iterator` that yields installed packages which were installed from official repositories.
+pub struct Officials<'a> {
+    /// Local package iterator.
+    locals: IntoIter<'a, Package<'a>>,
+    /// Sync DBs.
+    syncs: AlpmList<'a, Db<'a>>,
+}
+
+impl<'a> Iterator for Officials<'a> {
+    type Item = Package<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.locals.next().and_then(|p| {
+            let name = p.name();
+            match self.syncs.iter().find_map(|db| db.pkg(name).ok()) {
+                None => self.next(),
+                Some(_) => Some(p),
+            }
+        })
+    }
+}
+
 /// An `Iterator` that yields installed packages which were installed from the
 /// AUR, or are otherwise not from official repositories.
 pub struct Foreigns<'a> {
@@ -49,7 +71,15 @@ pub fn orphans(alpm: &Alpm) -> Vec<Package> {
         .collect()
 }
 
-// TODO Consider replacing this with a call to `alpm_utils::DbListExt::pkgs`.
+/// All official packages as an `Iterator`.
+pub fn officials(alpm: &Alpm) -> Officials {
+    let locals = alpm.localdb().pkgs().into_iter();
+    let syncs = alpm.syncdbs();
+    Officials { locals, syncs }
+}
+
+// TODO Consider replacing this with a call to `alpm_utils::DbListExt::pkgs`
+// and returning a `impl Iterator` trait object instead.
 /// All foreign packages as an `Iterator`.
 pub fn foreigns(alpm: &Alpm) -> Foreigns {
     let locals = alpm.localdb().pkgs().into_iter();
