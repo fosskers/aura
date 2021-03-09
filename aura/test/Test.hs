7525ccc215dc6418293342c05f44c0ea818c3ef5
Colin Woodbury <colin@fosskers.ca>
2021-03-09 15:45:56 -0800

    [aura] Fix `pacman.conf` line parsing logic - fixes #697

--- aura/test/Test.hs
+++ aura/test/Test.hs
@@ -59,8 +59,11 @@ suite conf badRaw goodRaw = testGroup "Unit Tests"
   , testGroup "Aura.Pacman"
     [ testCase "Parsing pacman.conf" $ do
         let p = parse config "pacman.conf" conf
-            r = either (const Nothing) (\(Config c) -> Just c) p >>= M.lookup "HoldPkg"
-        r @?= Just ["pacman", "glibc"]
+            r = either (const Nothing) (\(Config c) -> Just c) p
+        (r >>= M.lookup "HoldPkg") @?= Just ["pacman", "glibc"]
+        (r >>= M.lookup "GoodLine") @?= Just ["good"]
+        (r >>= M.lookup "BadLine") @?= Just ["bad"]
+        (r >>= M.lookup "OkLine") @?= Just ["ok"]
     ]
   , testGroup "Aura.Languages"
     [ testCase "Language names are complete" $ do
