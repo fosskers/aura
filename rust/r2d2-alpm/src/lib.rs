311de46ca7c9d4acbd533668811a27188112e166
Colin Woodbury <colin@fosskers.ca>
2022-02-02 19:30:13 -0800

    [r2d2] Docs and convenience methods

--- rust/r2d2-alpm/src/lib.rs
+++ rust/r2d2-alpm/src/lib.rs
@@ -1,16 +1,61 @@
-//! Types and utilities for the spawing of an R2D2 [`Pool`] that handles
+//! Types and utilities for the spawing of a [`r2d2::Pool`] that handles
 //! multiple connections to an Arch Linux [`Alpm`] database.
+//!
+//! # Usage
+//!
+//! To create a `Pool` that delegates [`Alpm`] connections within (for instance)
+//! some Rayon threads:
+//!
+//! ```
+//! use r2d2::Pool;
+//! use r2d2_alpm::AlpmManager;
+//! use rayon::prelude::*;
+//!
+//! let mngr = AlpmManager::from_file("/etc/pacman.conf").unwrap();
+//! let pool = Pool::builder().max_size(4).build(mngr).unwrap();
+//!
+//! (0..10).into_par_iter().for_each_with(pool, |p, n| {
+//!     // `Pool::get` will wait for a configurable length
+//!     // of time for a free connection before giving up.
+//!     if let Ok(alpm) = p.get() {
+//!         // Use the ALPM handle freely here.
+//!     }
+//! });
+//! ```
+//!
+//! Like [`std::sync::Arc`], `Pool` is cheap to [`Clone`], making it ideal for a
+//! number of Rayon methods like `map_with`, etc.
 
 #![warn(missing_docs)]
 
 use alpm::Alpm;
 use pacmanconf::Config;
 use r2d2::ManageConnection;
+use std::path::Path;
 
 /// Manage multiple [`Alpm`] handles.
 pub struct AlpmManager {
     /// The result of having parsed a `pacman.conf` file.
     config: Config,
+}
+
+impl AlpmManager {
+    /// Construct a new `AlpmManager` from a [`Config`] you've somehow obtained.
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+
+    /// Try to construct a new `AlpmManager` by reading a `pacman.conf` found at
+    /// the given filepath.
+    pub fn from_file<P>(path: P) -> Result<Self, pacmanconf::Error>
+    where
+        P: AsRef<Path>,
+    {
+        let config = Config::from_file(path)?;
+        let manager = Self { config };
+
+        Ok(manager)
+    }
 }
 
 impl ManageConnection for AlpmManager {
