f18b6c94176caa31074628a96b09fd47762264fd
Ryan DV <rdevilla@uwaterloo.ca>
2022-04-22 23:58:24 -0400

    setuid and setgid with given user for `git pull`

--- aura/lib/Aura/Build.hs
+++ aura/lib/Aura/Build.hs
@@ -38,6 +38,7 @@ import qualified RIO.Set as S
 import qualified RIO.Text as T
 import           RIO.Time
 import           System.Process.Typed
+import           System.Posix.User
 
 ---
 
@@ -194,7 +195,10 @@ pullRepo usr = do
   logDebug "git: Clearing worktree. "
   void . runProcess . setStderr closed . setStdout closed $ proc "git" ["reset", "--hard", "HEAD"]
   logDebug "git: Pulling repo."
-  ec <- runProcess . setStderr closed . setStdout closed $ proc "git" ["pull"]
+  ue <- liftIO . getUserEntryForName . T.unpack . user $ usr
+  let uid = userID ue
+  let gid = userGroupID ue
+  ec <- runProcess . setChildUser uid . setChildGroup gid . setStderr closed . setStdout closed $ proc "git" ["pull"]
   case ec of
     ExitFailure _ -> pure . Left . Failure $ FailMsg buildFail_12
     ExitSuccess   -> liftIO (chown usr "." ["-R"]) $> Right ()
