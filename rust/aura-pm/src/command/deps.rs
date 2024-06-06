33d48637b02bf1d5d27ff4d3c88c135410ebfb7f
Colin Woodbury <colin@fosskers.ca>
2024-06-07 05:27:54 +0900

    fix(deps): pass `syncdbs` instead
    This has the effect of making optional dependencies which are not
    installed also appear in the dep graph.

--- rust/aura-pm/src/command/deps.rs
+++ rust/aura-pm/src/command/deps.rs
@@ -6,12 +6,12 @@ use r2d2_alpm::Alpm;
 /// Given some packages to focus on, print their combined dependency graph in
 /// DOT format.
 pub(crate) fn graph(alpm: &Alpm, limit: Option<u8>, optional: bool, packages: Vec<String>) {
-    let db = alpm.as_ref().localdb();
+    let db = alpm.as_ref().syncdbs();
     let pkgs: Vec<_> = packages.iter().map(|p| p.as_ref()).collect();
     let foreigns: Vec<_> = aura_core::foreign_packages(alpm)
         .map(|p| p.name())
         .collect();
-    let graph = deps::PkgGraph::by_deps(db, limit, optional, &foreigns, &pkgs);
+    let graph = deps::PkgGraph::by_deps(&db, limit, optional, &foreigns, &pkgs);
 
     println!("{}", graph);
 }
@@ -19,12 +19,12 @@ pub(crate) fn graph(alpm: &Alpm, limit: Option<u8>, optional: bool, packages: Ve
 /// Like [`graph`], but display all packages that depend on the given ones
 /// instead.
 pub(crate) fn reverse(alpm: &Alpm, limit: Option<u8>, optional: bool, packages: Vec<String>) {
-    let db = alpm.as_ref().localdb();
+    let db = alpm.as_ref().syncdbs();
     let pkgs: Vec<_> = packages.iter().map(|p| p.as_ref()).collect();
     let foreigns: Vec<_> = aura_core::foreign_packages(alpm)
         .map(|p| p.name())
         .collect();
-    let graph = deps::PkgGraph::by_parents(db, limit, optional, &foreigns, &pkgs);
+    let graph = deps::PkgGraph::by_parents(&db, limit, optional, &foreigns, &pkgs);
 
     println!("{}", graph);
 }
